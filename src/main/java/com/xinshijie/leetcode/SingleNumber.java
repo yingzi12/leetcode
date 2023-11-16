6e6c83cf3171317f506c03193904fd9389d29840
卢煌 <luhuang@jtexpress.com>
2023-11-16 22:09:54 +0800

    dd

---
+++ src/main/java/com/xinshijie/leetcode/SingleNumber.java
@@ -0,0 +1,32 @@
+package com.xinshijie.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SingleNumber {
+    public static  int[] rotate(int[] nums) {
+        List<Integer> arrSigle=new ArrayList<>();
+        List<Integer> arrTwo=new ArrayList();
+
+        for(int i=0;i<nums.length;i++){
+            if(arrSigle.contains(Integer.valueOf(nums[i]))){
+                arrSigle.remove(Integer.valueOf(nums[i]));
+                arrTwo.add(nums[i]);
+            }else{
+                if(!arrTwo.contains(Integer.valueOf(nums[i]))){
+                    arrSigle.add(nums[i]);
+                }
+            }
+        }
+        int value[]=new int[2];
+        value[0]=arrSigle.get(0);
+        value[1]=arrSigle.get(1);
+        return value;
+    }
+
+    public static void main(String args[]){
+        int[] nums = {4,4,3,4,5,5};
+        System.out.println(rotate(nums));
+    }
+}
